73dc2185d30d58ab7f86282c0092b06dc10f1f13
BenSchuddeboom <Benzel@live.nl>
2019-03-20 16:22:56 +0100

    added basic DOM modification using js

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,4 +1,20 @@
 function submitComment() {
-    console.log("working");
-}
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
 
+    const comment = document.createElement('section');
+    const commentName = document.createElement('h3');
+    const commentContent = document.createElement('p');
+
+    commentName.innerHTML = `${name} said:`;
+    commentContent.innerHTML = msg;
+
+    comment.classList.add("comment");
+
+    comment.appendChild(commentName);
+    comment.appendChild(commentContent);
+
+    console.log(comment)
+}
